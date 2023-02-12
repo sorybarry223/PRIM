1feb623e0945094aa4415fdb4da4ad330b396cd9
Sory Ibrahima Barry <sorybarry@Sorys-MacBook-Air.local>
2023-02-12 17:22:07 +0100

    almost

---
+++ My_implementation/Python simulation/system2.py
@@ -0,0 +1,22 @@
+import csv
+import random
+
+def generate_dataset(rows, columns):
+    with open('system2.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # write the header row
+        header = ['CacheLevel', 'AccesTime', 'Status']
+        writer.writerow(header)
+        
+        # write the data rows
+        for i in range(rows):
+            cache = random.choice(['L1', 'L2','L3'])
+            access_time = random.randint(1,5)
+            status = random.choice(['Leaked', 'NLeaked','Idle'])
+            
+            row = [cache, access_time, status]
+            writer.writerow(row)
+
+# generate a dataset with 500 rows and 3 columns
+generate_dataset(500, 3)
